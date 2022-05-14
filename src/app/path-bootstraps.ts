06afca7b51a9e50414d55342ff433e8569e677f6
“Sardor” <xudjamovsardor12@gmail.com>
2022-05-14 16:21:39 +0500

    Sitting booststrap

---
+++ src/app/path-bootstraps.ts
@@ -0,0 +1,14 @@
+export const path  = [
+    "../../../assets/css/bootstrap.min.css",
+    "../../../assets/css/jquery-te-1.4.0.css",
+    "../../../assets/css/intlTelInput.min.css",
+    "../../../assets/css/jquery.fancybox.min.css",
+    "../../../assets/css/leaflet.css",
+    "../../../assets/css/line-awesome.css",
+    "../../../assets/css/owl.carousel.min.css",
+    "../../../assets/css/owl.theme.default.min.css",
+    "../../../assets/css/selectize.css",
+    "../../../assets/css/style.css",
+    "../../../assets/css/upvotejs.min.css",
+    "../../../assets/css/bootstrap.css",
+]
